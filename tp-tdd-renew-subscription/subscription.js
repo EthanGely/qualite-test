76dbd8fbb44f9c105b623e80352f7158a0af52ea
Ethan <ethangely@free.fr>
2025-06-06 12:09:35 +0200

    added logic expired

--- tp-tdd-renew-subscription/subscription.js
+++ tp-tdd-renew-subscription/subscription.js
@@ -3,6 +3,7 @@ function canRenewSubscription(subscription, currentDate) {
     if (subscription.hasBeenRenewed) return false;
     if (subscription.unpaidDebt) return false;
     if (subscription.isTrial) return false;
+    if (subscription.status !== 'active') return false;
     return true;
 }
 
