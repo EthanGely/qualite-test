f9234648cb995e99cd8beba3a9525d1f37fb6148
Ethan <ethangely@free.fr>
2025-06-06 12:07:09 +0200

    added debt test for renewal

--- tp-tdd-renew-subscription/subscription.test.js
+++ tp-tdd-renew-subscription/subscription.test.js
@@ -11,4 +11,16 @@ describe("canRenewSubscription", () => {
     };
     expect(canRenewSubscription(subscription)).toBe(false);
   });
+
+  //Should not allow renewal if there is an unpaid debt
+  test("should not allow renewal if there is an unpaid debt", () => {
+    const subscription = {
+      status: 'active',
+      endDate: '2099-12-31',
+      hasBeenRenewed: false,
+      unpaidDebt: true,
+      isTrial: false,
+    };
+    expect(canRenewSubscription(subscription)).toBe(false);
+  });
 });
