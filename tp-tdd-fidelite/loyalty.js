31fe9a41e97b27bb19e64ed19f8b4005c00f9b0d
Ethan <ethangely@free.fr>
2025-06-06 11:00:54 +0200

    created loyalty function

--- tp-tdd-fidelite/loyalty.js
+++ tp-tdd-fidelite/loyalty.js
@@ -1,5 +1,7 @@
 function calculateLoyaltyPoints(cart) {
-
+    return cart.reduce((points, item) => {
+        return points + Math.floor(item.price / 10);
+    }, 0);
 }
 
 module.exports = { calculateLoyaltyPoints };
--- tp-tdd-fidelite/loyalty.test.js
+++ tp-tdd-fidelite/loyalty.test.js
@@ -1,15 +1,14 @@
-const {calculateLoyaltyPoints} = require('./loyalty.js');
+const { calculateLoyaltyPoints } = require("./loyalty.js");
 
-describe('standardProduct', () => {
-    test('should return 1 point for every 10€ spent', () => {
-        expect(calculateLoyaltyPoints(10)).toBe(1);
-        expect(calculateLoyaltyPoints(20)).toBe(2);
-        expect(calculateLoyaltyPoints(30)).toBe(3);
-    });
+describe("standardProduct", () => {
+  test("should return 1 point for every 10€ spent", () => {
+    expect(calculateLoyaltyPoints([{type: "standard", price: 10 }])).toBe(1);
+    expect(calculateLoyaltyPoints([{type: "standard", price: 20 }])).toBe(2);
+    expect(calculateLoyaltyPoints([{type: "standard", price: 30 }])).toBe(3);
+  });
 
-    test('should return 0 points for less than 10€', () => {
-        expect(calculateLoyaltyPoints(9.99)).toBe(0);
-        expect(calculateLoyaltyPoints(5)).toBe(0);
-    });
-}
-);
+  test("should return 0 points for less than 10€", () => {
+    expect(calculateLoyaltyPoints([{type: "standard", price: 9.99 }])).toBe(0);
+    expect(calculateLoyaltyPoints([{type: "standard", price: 5 }])).toBe(0);
+  });
+});
