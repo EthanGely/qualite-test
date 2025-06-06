af707c4975862dc0d9d47bd443a0bfed5941d9b3
Ethan <ethangely@free.fr>
2025-06-06 12:03:58 +0200

    added first test for renewal

--- tp-tdd-renew-subscription/subscription.test.js
+++ tp-tdd-renew-subscription/subscription.test.js
@@ -1,1 +1,14 @@
 const { canRenewSubscription } = require("./subscription.js");
+
+describe("canRenewSubscription", () => {
+  test("should not allow renewal if already renewed", () => {
+    const subscription = {
+      status: 'active',
+      endDate: '2099-12-31',
+      hasBeenRenewed: true,
+      unpaidDebt: false,
+      isTrial: false,
+    };
+    expect(canRenewSubscription(subscription)).toBe(false);
+  });
+});
