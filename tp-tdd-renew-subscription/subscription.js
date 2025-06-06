eb06dec495652e7f5fa6bba426de31799356372a
Ethan <ethangely@free.fr>
2025-06-06 12:07:42 +0200

    added logic debt

--- tp-tdd-renew-subscription/subscription.js
+++ tp-tdd-renew-subscription/subscription.js
@@ -1,6 +1,7 @@
 function canRenewSubscription(subscription, currentDate) {
 
     if (subscription.hasBeenRenewed) return false;
+    if (subscription.unpaidDebt) return false;
     return true;
 }
 
